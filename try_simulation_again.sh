35a5b27974417578a7db4f724f0637ee2d877f52
AtlantaEmrys2002 <milli@MacBookAir.localdomain>
2026-07-02 18:51:44 +0100

    Building infinite statistics maps.

--- try_simulation_again.sh
+++ try_simulation_again.sh
@@ -54,20 +54,9 @@ conda activate fermi
 
 # gtexpcube2 infile=$FERMILIVETIMECUBE cmap=$FERMIBINNED outfile=$FERMIEXPMAP irfs=P8R3_ULTRACLEANVETO_V3 enumbins=5 emin=300 emax=200000 binsz=0.23 evtype=3
 
-
-
-
-
-
-
-
-
-
-## Generate the PSF for handling pointlike sources - want at l = 90 degrees, b = 40 degrees - converted to RA DEC J2000
-## which produces 252.131996, 41.585827°
-# gtpsf expcube=$FERMIEXPMAP outfile=$POINTSOURCEPSF irfs=P8R3_ULTRACLEANVETO_V3 ra=252.131996 dec=41.585827 emin=300 emax=200000 nenergies=16 thetamax=30 ntheta=300
-#
-### Generate the PSF for handling background diffuse sources - same location as for pointlike
+# Generate the PSF for handling pointlike sources - want at l = 90 degrees, b = 40 degrees - converted to RA DEC J2000
+# which produces 252.131996, 41.585827°
+gtpsf expcube=$FERMILIVETIMECUBE outfile=$POINTSOURCEPSF irfs=P8R3_ULTRACLEANVETO_V3 ra=252.131996 dec=41.585827 emin=300 emax=200000 nenergies=5 thetamax=30 ntheta=300 evtype=3
 
 
 # Deactivate Fermi environment
