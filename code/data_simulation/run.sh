08d886dff9f3dc649723e7c183e3308ce18ff91e
AtlantaEmrys2002 <milli@MacBookAir.localdomain>
2026-08-14 10:25:10 +0100

    Resetting data simulation pipeline.

--- code/data_simulation/run.sh
+++ code/data_simulation/run.sh
@@ -6,13 +6,7 @@ source gamma_source_detection/bin/activate
 # python create_mock_catalogs.py --catalog=/Volumes/T7/data/catalog/4FGL_DR4.fit --number=10 --analyse=yes --verify=first
 
 # Create mock all-sky count maps for AGN, pulsar, and background
-# python create_mock_skymaps.py --exposure_fits=/Volumes/T7/project_data/real_data/fermi_filtered_gti_exposure_map.fits --number_catalogs=10 --pointsource_psf=/Volumes/T7/project_data/real_data/pointsource_psf.fits --diffuse_source_psf_roi=/Volumes/T7/project_data/real_data/diffuse_psf_roi/count_map.fits --isotropic_background=/Volumes/T7/data/background_models/iso_P8R3_SOURCE_V3_v1.txt --galactic_background=/Volumes/T7/data/background_models/gll_iem_v07.fits --num_maps_per_catalog=10
-
-# python create_mock_skymaps_new.py --exposure_fits=/Volumes/T7/project_data/real_data/fermi_filtered_gti_exposure_map.fits --number_catalogs=1 --pointsource_psf=/Volumes/T7/project_data/real_data/pointsource_psf.fits --diffuse_source_psf_roi=/Volumes/T7/project_data/real_data/diffuse_psf_roi/count_map.fits --isotropic_background=/Volumes/T7/data/background_models/iso_P8R3_SOURCE_V3_v1.txt --galactic_background=/Volumes/T7/data/background_models/gll_iem_v07.fits --num_maps_per_catalog=1
-#
-
-python create_dataset_2.py --num_catalogs=1 --num_skymaps_per_catalog=1 --num_energy_bins=5 --patch_directory=./simulated_data
-
+python create_mock_skymaps.py --exposure_fits=/Volumes/T7/project_data/real_data/fermi_filtered_gti_exposure_map.fits --number_catalogs=10 --pointsource_psf=/Volumes/T7/project_data/real_data/pointsource_psf.fits --diffuse_source_psf_roi=/Volumes/T7/project_data/real_data/diffuse_psf_roi/count_map.fits --isotropic_background=/Volumes/T7/data/background_models/iso_P8R3_SOURCE_V3_v1.txt --galactic_background=/Volumes/T7/data/background_models/gll_iem_v07.fits --num_maps_per_catalog=10
 
 # python create_dataset.py --num_catalogs=10 --num_skymaps_per_catalog=10 --num_energy_bins=5 --patch_directory=./simulated_data
 
